6a27a37b2c8cf787d48fa697861364cc58554794
innsd <innsd@qq.com>
2018-03-28 16:01:51 +0800

    count of words

--- count_word.py
+++ count_word.py
@@ -1,6 +1,7 @@
 import error_handling
 import handle_word
 import re
+import operator
 filename='testfile.dat'
 contents=''
 try:
@@ -17,3 +18,15 @@ print(word_list)
 word_list=list(filter(handle_word.is_normal_word,word_list))
 handle_word.standardizing(word_list)
 print(word_list)
+word_dict={}
+for word in word_list:
+    try:
+        word_dict[word]+=1
+    except KeyError as e:
+        word_dict[word]=1
+print(word_dict)
+sorted_words=sorted(word_dict.items(),key=operator.itemgetter(1),reverse=True)
+for t in sorted_words:
+    print(t)
+    #接下来的计划：使用API进行翻译
+    #词源归纳
