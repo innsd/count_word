f6bcab78c7fd918aa733fcf1fd56c7d9e83ffe80
innsd <innsd@qq.com>
2018-04-04 09:51:03 +0800

    change the word_ditc strutc

--- count_word.py
+++ count_word.py
@@ -1,5 +1,6 @@
 import error_handling
 import handle_word
+import c_io
 import re
 import operator
 filename='testfile.dat'
@@ -19,14 +20,16 @@ word_list=list(filter(handle_word.is_normal_word,word_list))
 handle_word.standardizing(word_list)
 print(word_list)
 word_dict={}
-for word in word_list:
+for word in word_list:#形成字典word_dict
     try:
-        word_dict[word]+=1
+        word_dict[word]['times']+=1
     except KeyError as e:
-        word_dict[word]=1
+        word_dict[word]={'times':1,'explains':[]}
 print(word_dict)
-sorted_words=sorted(word_dict.items(),key=operator.itemgetter(1),reverse=True)
-for t in sorted_words:
-    print(t)
+handle_word.translate(word_dict)#使用有道API翻译
+sorted_words=dict(sorted(list(word_dict.items()),key=lambda item:item[1]['times'],reverse=True))
+print(sorted_words)
+print(repr(sorted_words))
+c_io.save_as_json(sorted_words)
     #接下来的计划：使用API进行翻译
     #词源归纳
